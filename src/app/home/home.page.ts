324579a96f93e9251ed4b0484566ade4987ab068
ahirsch-github <anja_hirsch@t-online.de>
2023-11-27 20:58:47 +0100

    Fix routing issue, add questionnaire component and tested cookies

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuestionnaireComponent } from '../components/questionnaire/questionnaire.component';
+import { ModalController } from '@ionic/angular';
+import { CookieService } from 'ngx-cookie-service';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  eventCat: string | 'test' | undefined;
 
-  constructor() {}
+  constructor(
+    private modalCtrl: ModalController,
+    private cookieService: CookieService,
+  ) {}
 
+  ngOnInit() {
+    this.showQuestionnaireIfNeeded()
+  }
+  
+  private persolanizeEventCat() {
+    const preferences = this.cookieService.get('userType');
+      console.log('Preferences: ', preferences);
+      this.eventCat = preferences;
+  }
+  
+  private async showQuestionnaireIfNeeded() {
+    console.log('Checking if questionnaire is needed');
+    const isFirstTime = localStorage.getItem('hasVisited') === null;
+    if (isFirstTime) {
+      const modal = await this.modalCtrl.create({
+      component: QuestionnaireComponent
+    });
+    
+    modal.onDidDismiss().then((data) => {
+      this.persolanizeEventCat();
+    });
+
+    return await modal.present();
+    } else {
+      this.persolanizeEventCat();
+    }
+  }
 }
