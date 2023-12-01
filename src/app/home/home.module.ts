b9657d7f7381158187b730f129f7361e60e66ff7
ahirsch-github <anja_hirsch@t-online.de>
2023-12-01 10:22:18 +0100

    fixed import

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,6 +6,7 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 import { SharedModuleModule } from '../shared-module/shared-module.module';
+import { QuestionnaireComponent } from '../components/questionnaire/questionnaire.component';
 
 
 @NgModule({
